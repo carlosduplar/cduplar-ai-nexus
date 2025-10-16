4a11dbecc5e0d16f96f5e51856daae48680be9b7
Carlos Mello <cduplar@gmail.com>
2025-10-16 15:59:50 +0200

    Fix useParams error in LanguageSelector

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
